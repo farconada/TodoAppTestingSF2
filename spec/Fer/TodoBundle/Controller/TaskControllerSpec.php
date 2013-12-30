4648c89f1d9eca9b580aa20d2bd2ac7c6692e1e8
Fernando A <fernando.arconada@gmail.com>
2013-12-30 10:21:52 +0100

    spec taskctrl tiene indexaction

--- spec/Fer/TodoBundle/Controller/TaskControllerSpec.php
+++ spec/Fer/TodoBundle/Controller/TaskControllerSpec.php
@@ -1,11 +1,24 @@
 <?php
+namespace spec\Fer\TodoBundle\Controller;
 /**
  * Created by PhpStorm.
  * User: fernando
  * Date: 30/12/13
  * Time: 10:02
  */
+use PhpSpec\ObjectBehavior;
+use Fer\TodoBundle\Entity\TaskRepository;
+use Fer\TodoBundle\Entity\Task;
 
-class TaskControllerSpec {
+class TaskControllerSpec extends ObjectBehavior {
+
+    public function it_should_be_a_symfony_controller() {
+        $this->shouldHaveType('Symfony\Bundle\FrameworkBundle\Controller\Controller');
+    }
+
+    public function it_should_have_index_action() {
+        $this->indexAction();
+    }
+
 
 } 
